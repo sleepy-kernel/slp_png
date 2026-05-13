5a6e0ec2fea8ca4e226b61f5847f7026d6b4c346
sleepy-kernel <222554062+sleepy-kernel@users.noreply.github.com>
2026-05-14 05:28:00 +0700

    avoid redefine macro on max/min macros

--- src/slp_image_transform/slp_image_transform.c
+++ src/slp_image_transform/slp_image_transform.c
@@ -43,8 +43,12 @@ limitations under the License.
 #include <emmintrin.h>
 #endif
 
+#ifndef max
 #define max(a, b) ((a) > (b) ? (a) : (b))
+#endif
+#ifndef min
 #define min(a, b) ((a) < (b) ? (a) : (b))
+#endif
 static int64_t ceil__(double x);
 
 
